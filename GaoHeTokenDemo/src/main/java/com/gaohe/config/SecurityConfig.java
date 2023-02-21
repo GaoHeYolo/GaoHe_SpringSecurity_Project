d4ebec4e11ad5e4ee6ed38f432e8bd1413168ad0
高赫 <gaoh@dongshuoit.com>
2023-02-21 15:19:39 +0800

    认证配置详讲

--- GaoHeTokenDemo/src/main/java/com/gaohe/config/SecurityConfig.java
+++ GaoHeTokenDemo/src/main/java/com/gaohe/config/SecurityConfig.java
@@ -25,16 +25,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                //关闭csrf
+//                .csrf().disable()
+//                //不通过Session获取SecurityContext
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeRequests()
+//                //对于登录接口 允许匿名访问
+//                .antMatchers("/user/login").anonymous()
+//                //除上面的所有请求全部需要鉴权认证
+//                .anyRequest().authenticated();
         http
-                //关闭csrf
                 .csrf().disable()
-                //不通过Session获取SecurityContext
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                //对于登录接口 允许匿名访问
-                .antMatchers("/user/login").anonymous()
-                //除上面的所有请求全部需要鉴权认证
+                .antMatchers("/hello").permitAll()
+                .antMatchers("user/login").anonymous()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
