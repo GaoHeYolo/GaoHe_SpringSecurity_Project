7d68c40b6128ecb2173db6079f74d625fb0638b4
高赫 <gaoh@dongshuoit.com>
2023-02-24 01:16:45 +0800

    认证失败处理器

--- GaoHeSecurityQuickStart/src/main/java/com/gaohe/config/SecurityConfig.java
+++ GaoHeSecurityQuickStart/src/main/java/com/gaohe/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
@@ -12,9 +13,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationSuccessHandler successHandler;
 
+    @Autowired
+    private AuthenticationFailureHandler failureHandler;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().successHandler(successHandler);
+        http.formLogin()
+                //配置认证成功处理器
+                .successHandler(successHandler)
+                //配置认证失败处理器
+                .failureHandler(failureHandler);
         http.authorizeRequests().anyRequest().authenticated();
     }
 }
