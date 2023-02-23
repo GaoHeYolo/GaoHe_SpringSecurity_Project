7496a00ce3710de251fdd624c97616670efb326c
高赫 <gaoh@dongshuoit.com>
2023-02-24 01:09:34 +0800

    认证成功处理器

---
+++ GaoHeSecurityQuickStart/src/main/java/com/gaohe/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.gaohe.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationSuccessHandler successHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().successHandler(successHandler);
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
